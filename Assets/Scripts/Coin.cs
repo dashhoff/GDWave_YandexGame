bd2b6161c0d697838d7d28aadc1f13b4325f45a4
Danila Shalyapin <dashoff@yandex.ru>
2025-01-05 19:57:09 +0400

    fix coin animation

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,6 +6,8 @@ public class Coin : MonoBehaviour
     [SerializeField] private Vector3 _endTrasform = new Vector3(1.1f, 1.1f, 1.1f);
     [SerializeField] private float _timeToEnd = 0.2f;
 
+    [SerializeField] private float _timeToZero = 0.2f;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
@@ -16,7 +18,7 @@ public class Coin : MonoBehaviour
     {
         DOTween.Sequence()
             .Append(transform.DOScale(_endTrasform, _timeToEnd))
-            .Append(transform.DOScale(0,0))
+            .Append(transform.DOScale(0, _timeToZero))
             .OnComplete(()=>
             {
                 Destroy(gameObject);
